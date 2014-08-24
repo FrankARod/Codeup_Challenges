03d7b4c45ff2cb50796d1e910e3d92e7bafb0578
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-24 16:34:52 -0500

    Finished game

--- blackjack.php
+++ blackjack.php
@@ -38,7 +38,8 @@ function cardIsAce($card) {
 // numeric cards are worth their value
 function getCardValue($card) {
   // todo
-	switch (substr($card, 0, 2)) {
+	$card_value = substr($card, 0, 2);
+	switch ($card_value) {
 		case 'A ': 
 			$value = 11;
 			break;
@@ -52,9 +53,11 @@ function getCardValue($card) {
 			$value = 10;
 			break;
 		default:
-			$value = $card;
+			$value = intval($card_value);
 			break; 
 	}
+	// var_dump(substr($card, 0, 2));
+	// var_dump($value);
 	return $value;
 }
 
@@ -67,22 +70,25 @@ function getHandTotal($hand) {
 	foreach ($hand as $card) {
 		$hand_value += getCardValue($card); 
 	}
-	if $hand_value > 21 {
+	if ($hand_value > 21) {
 		$hand_value = 0;
 		foreach ($hand as $card) {
-			if cardIsAce($card) {
+			if (cardIsAce($card)) {
 				$hand_value += 1;
 			} else {
 				$hand_value += getCardValue($card);
 			}
 		}
 	}
+	return $hand_value;
 }
 
 // draw a card from the deck into a hand
 // pass by reference (both hand and deck passed in are modified)
 function drawCard(&$hand, &$deck) {
-  // todo
+	$new_card = array_splice($deck, array_rand($deck), 1);
+	$hand[] = $new_card[0];
+	// return $hand;
 }
 
 // print out a hand of cards
@@ -93,9 +99,22 @@ function drawCard(&$hand, &$deck) {
 // or:
 // Player: [J D] [2 D] Total: 12
 function echoHand($hand, $name, $hidden = false) {
-  // todo
+  echo "$name's hand:" . PHP_EOL;
+  $handString = "";
+  if ($hidden) {
+  	$handString .= $hand[0] . PHP_EOL;
+  	} else {
+  	foreach ($hand as $key => $card) {
+  	$handString .= $card . PHP_EOL;
+  	}
+  }
+  return $handString;
 }
 
+function get_input($single_letter = FALSE) {
+   //returns single letter input for menus or full strings for any other application
+   return ($single_letter ? substr(strtoupper(trim(fgets(STDIN))), 0, 1) : trim(fgets(STDIN)));  
+}
 // build the deck of cards
 $deck = buildDeck($suits, $cards);
 
@@ -105,24 +124,74 @@ $player = [];
 
 // dealer and player each draw two cards
 // todo
+drawCard($dealer, $deck);
+drawCard($dealer, $deck);
+
+drawCard($player, $deck);
+drawCard($player, $deck);
 
 // echo the dealer hand, only showing the first card
 // todo
+echo echoHand($dealer, "Dealer", true);
 
 // echo the player hand
 // todo
+// echo echoHand($player, "Player", false);
 
 // allow player to "(H)it or (S)tay?" till they bust (exceed 21) or stay
-while (todo) {
+$hand_value = 0;
+while ($hand_value < 21) {
   // todo
+	echo echoHand($player, "Player");
+	echo "(H)it or (S)tay?" . PHP_EOL;
+	switch (get_input(true)) {
+		case 'H':
+			drawCard($player, $deck);
+			$hand_value = getHandTotal($player);
+			break;
+
+		case 'S':
+			$hand_value = 22;
+			break;
+		
+		default:
+			echo "Invalid Input" . PHP_EOL;
+			break;
+	}
 }
 
 // show the dealer's hand (all cards)
 // todo
+echo echoHand($dealer, "Dealer");
 
 // todo (all comments below)
 
 // at this point, if the player has more than 21, tell them they busted
+$player_hand_value = getHandTotal($player);
+if ($player_hand_value > 21) {
+	echo "You busted!" . PHP_EOL;
+} elseif ($player_hand_value == 21) {
+	echo "You got 21! You win!" . PHP_EOL;
+} else {
+	$dealer_hand_value = 0;
+	while ($dealer_hand_value <= 17) {
+		drawCard($dealer, $deck);
+		echo echoHand($dealer, "Dealer");
+		usleep(5000000);
+		$dealer_hand_value = getHandTotal($dealer);
+		// var_dump($dealer_hand_value);
+	}
+	echo echoHand($dealer, "Dealer");
+	if ($dealer_hand_value > 21) {
+		echo "Dealer busted! You win!" . PHP_EOL;
+	} elseif ($player_hand_value == $dealer_hand_value) {
+		echo "It's a push! No one wins!" . PHP_EOL;
+	} elseif ($player_hand_value < $dealer_hand_value) {
+		echo "Dealer wins!" . PHP_EOL;
+	} else {
+		echo "Player wins!" . PHP_EOL;
+	}
+}
 // otherwise, if they have 21, tell them they won (regardless of dealer hand)
 
 // if neither of the above are true, then the dealer needs to draw more cards
